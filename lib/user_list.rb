869f18be0cc31c703acfb264fb270d737d09d8b9
Jordan Corley <rainwolfj@gmail.com>
2016-03-23 07:37:57 -0500

    Added ids, cleaned user_list, all outputs seem good

--- lib/user_list.rb
+++ lib/user_list.rb
@@ -1,10 +1,11 @@
-USERS_LIST = [{:first_name => "Greg", :last_name => "Henry", :age => 34},
-              {:first_name => "Mary", :last_name => "Beth", :age => 45},
-              {:first_name => "Gina", :last_name => "Sue", :age => 66},
-              {:first_name => "David", :last_name => "Illheim", :age => 12},
-              {:first_name => "Ward", :last_name => "Deverson", :age => 24},
-              {:first_name => "Miles", :last_name => "Guster", :age => 72},
-              {:first_name => "Samael", :last_name => "Ouver", :age => 99},
-              {:first_name => "Chuck", :last_name => "Testa", :age => 31},
-              {:first_name => "Dwayne", :last_name => "Johnson", :age => 50},
-              {:first_name => "Tooth", :last_name => "Fairy", :age => 83},]
+
+USERS_LIST = [{:first_name => "Greg",   :last_name => "Henry",    :age => 34, :id => 1},
+              {:first_name => "Mary",   :last_name => "Beth",     :age => 45, :id => 2},
+              {:first_name => "Gina",   :last_name => "Sue",      :age => 66, :id => 3},
+              {:first_name => "David",  :last_name => "Illheim",  :age => 12, :id => 4},
+              {:first_name => "Ward",   :last_name => "Deverson", :age => 24, :id => 5},
+              {:first_name => "Miles",  :last_name => "Guster",   :age => 72, :id => 6},
+              {:first_name => "Samael", :last_name => "Ouver",    :age => 99, :id => 7},
+              {:first_name => "Chuck",  :last_name => "Testa",    :age => 31, :id => 8},
+              {:first_name => "Dwayne", :last_name => "Johnson",  :age => 50, :id => 9},
+              {:first_name => "Tooth",  :last_name => "Fairy",    :age => 83, :id => 10},]
